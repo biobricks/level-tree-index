5e7936449c7ecfb626bf5c8935193ca91a82fcb6
Yardena Cohen <yardenack@gmail.com>
2017-04-28 15:35:54 -0700

    tape-ify: children.js
    somehow this works, but now move doesn't work. what is going on here?

---
+++ tests/children.js
@@ -0,0 +1,27 @@
+var base = require('../examples/base.js');
+var tape = require('tape');
+
+tape('children', function(t) {
+    t.plan(1)
+    base(function(err, db, tree, cb) {
+        if(err) t.fail("mysterious failure A: " + err)
+        tree.children('foo', function(err, children) {
+            if(err) t.fail("mysterious failure B: " + err)
+            t.deepEqual(children,[{
+                value: { parentKey: '1', name: 'bar' },
+                path: 'foo.bar',
+                key: '2'
+            },{
+                value: { parentKey: '2', name: 'baz' },
+                path: 'foo.bar.baz',
+                key: '3' 
+            },{
+                value: { parentKey: '1', name: 'cat' },
+                path: 'foo.cat',
+                key: '4'
+            }],"objects as expected")
+            cb()
+            t.end()
+        })
+    })
+})
