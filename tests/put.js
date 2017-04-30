3f4eb720ed27745d8afb5c38189132ba9c334a8b
Yardena Cohen <yardenack@gmail.com>
2017-04-29 17:13:03 -0700

    tape-ify: put.js

---
+++ tests/put.js
@@ -0,0 +1,16 @@
+var base = require('../examples/base.js');
+var tape = require('tape');
+
+tape('put', function(t) {
+    t.plan(2)
+    base(function(err,db,tree,cb) {
+        tree.getFromPath('foo.bar.baz', function(err, key, value) {
+            if(err) t.fail("mysterious failure A: " + err)
+            t.equal(key,"3","key")
+            t.deepEqual(value,{
+                parentKey: '2',
+                name: 'baz'
+            },"value")
+        })
+    })
+})
