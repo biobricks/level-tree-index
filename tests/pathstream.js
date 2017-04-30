542b78c4fcab5694368ea03409e485ac5eb6854d
Yardena Cohen <yardenack@gmail.com>
2017-04-29 17:29:04 -0700

    tape-ify: pathstream.js

---
+++ tests/pathstream.js
@@ -0,0 +1,22 @@
+var base = require('../examples/base.js');
+var tape = require('tape');
+
+tape('pathStream', function(t) {
+    t.plan(6)
+    base(function(err, db, tree, cb) {
+        if(err) t.fail("mysterious failure A: " + err)
+        var s = tree.pathStream('foo');
+        s.on('data', function(path) {
+            if (t.equal(typeof path, "string", "it's a string: " + path)) {return}
+            switch(path) {
+            case "foo.bar": t.pass(path); break
+            case "foo.bar.baz": t.pass(path); break
+            case "foo.cat": t.pass(path); break
+            default: t.fail("path should not be this: " + path)
+            }
+        })
+        s.on('error', function(err) {
+            if(err) t.fail("mysterious failure B: " + err)
+        })
+    })
+})
