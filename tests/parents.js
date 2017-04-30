5378a08704b8fbef32acc40bb96cca4e31c188fc
Yardena Cohen <yardenack@gmail.com>
2017-04-29 17:44:16 -0700

    tape-ify: parents.js

---
+++ tests/parents.js
@@ -0,0 +1,20 @@
+var base = require('../examples/base.js');
+var tape = require('tape');
+
+tape('parents', function(t) {
+    t.plan(1)
+    base(function(err, db, tree, cb) {
+        if(err) t.fail("mysterious failure A: " + err)
+        tree.parents('foo.bar.baz', {
+            height: 1,
+            includeCurrent: false
+        }, function(err, parents) {
+            if(err) t.fail("mysterious failure B: " + err)
+            t.deepEqual(parents,[{
+                value: { parentKey: '1', name: 'bar' },
+                key: '2',
+                path: 'foo.bar'
+            }],"parents")
+        })
+    })
+})
