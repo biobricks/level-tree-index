81c4cf7425052f499b0edfba48095a3b17cbb2c6
Yardena Cohen <yardenack@gmail.com>
2017-05-05 09:45:08 -0700

    tape-ify: levelup.js

---
+++ tests/levelup.js
@@ -0,0 +1,25 @@
+var base = require('../examples/base.js');
+var tape = require('tape');
+
+tape('levelup', function(t) {
+    t.plan(2)
+    base(function(err,db,tree,cb) {
+        tree.getFromPath('foo.bar.baz', function(err, key, value) {
+            if(err) t.fail("mysterious failure A: " + err)
+            t.equal(key,"3","key")
+            t.deepEqual(JSON.parse(value),{
+                parentKey: '2',
+                name: 'baz'
+            },"value")
+        })
+    },{
+        levelup: true,
+        pathProp: function(value) {
+            return JSON.parse(value).name;
+        },
+        parentProp: function(value) {
+            return JSON.parse(value).parentKey;
+        }
+    })
+})
+
